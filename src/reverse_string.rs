12a61eda676f034377caa5a6878c2f72db0806af
Jason Schein <jasonschein@gmail.com>
2015-04-16 00:30:08 -0700

    Reverse string more efficient

--- src/reverse_string.rs
+++ src/reverse_string.rs
@@ -1,15 +1,16 @@
+// Need to comment this out because its unstable and cannot run on beta (only nightly)
+// The feature unicode is required for graphemes
+//#![feature(unicode)]
 
-pub fn reverse_string(stringToReverse: String) -> String {
-    let mut chars = stringToReverse.chars();
-    let mut reversed: Vec<char> = Vec::new();
-
-    for c in chars {
-        reversed.push(c);
-    }
+pub fn reverse_string(string_to_reverse: String) -> String {
+    let chars = string_to_reverse.chars();
+    let mut char_stack: Vec<_> = string_to_reverse.chars().collect();
 
     let mut answer = String::new();
+    
+    // Reversing a string without using a built in reverse function
     loop {
-        let top = match reversed.pop() {
+        let top = match char_stack.pop() {
             None => break, // empty
             Some(c) => c,
         };
@@ -18,3 +19,7 @@ pub fn reverse_string(stringToReverse: String) -> String {
 
     return answer;
 }
+
+// pub fn reverse_string_graphemes(string_to_reverse: String) -> String {
+//     string_to_reverse.graphemes(true).rev().collect()
+// }
